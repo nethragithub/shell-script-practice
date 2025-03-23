d4e57b3d70b9a4a5fa2c4f84b6d91b3a51ad30c6
nethragithub <nethra.sangeetham@gmail.com>
2025-03-23 15:20:10 +0530

    initial commit

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -5,7 +5,7 @@ echo "$0 ------ name of the script"
 echo "$HOME ----- home dir of user"
 echo "$USER --- user who ran script"
 echo "$PWD --- present working directory"
-echo "$HOSTNMAE --- server hostname"
+echo "$HOSTNAME --- server hostname"
 sleep 60 &
 echo "$$ ---- pid of current shell script"
 echo "$! ---- pid of last background command"
