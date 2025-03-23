2fdb1293ba2258d5cef6967a66d48ad9e7fdde86
nethragithub <nethra.sangeetham@gmail.com>
2025-03-23 16:51:45 +0530

    initial commit

--- 11-monitor-memory.sh
+++ 11-monitor-memory.sh
@@ -9,7 +9,7 @@ do
   FOLDER=$(echo $line | awk -F " " '{print $NF}')
   if [ $USAGE -ge $DISK_THRESHOLD ]
   then
-      MESSAGE+="$FOLDER is more than $DISk_THRESHOLD, current usage is:: $USAGE \n"
+      MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, current usage is:: $USAGE \n"
   fi
 done <<<$DISK_USAGE
 echo -e "message:: $MESSAGE"
